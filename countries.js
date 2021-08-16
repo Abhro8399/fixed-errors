d491ce18a925b03a5f1becefdc75e92afdca45b3
Abhro8399 <88388598+Abhro8399@users.noreply.github.com>
2021-08-16 09:45:40 +0530

    Add files via upload

---
+++ countries.js
@@ -0,0 +1,48 @@
+var country_arr = new Array("Afghanistan","China", "India","Pakistan","USA");  //array for country
+
+// States
+var s_a = new Array();
+s_a[0]=null;
+s_a[1]="Badakhshan|Badghis|Baghlan|Balkh|Bamian|Farah|Faryab|Ghazni|Ghowr|Helmand|Herat|Jowzjan|Kabol|Kandahar|Kapisa|Konar|Kondoz|Laghman|Lowgar|Nangarhar|Nimruz|Oruzgan|Paktia|Paktika|Parvan|Samangan|Sar-e Pol|Takhar|Vardak|Zabol";
+s_a[2]="Anhui|Beijing|Chongqing|Fujian|Gansu|Guangdong|Guangxi|Guizhou|Hainan|Hebei|Heilongjiang|Henan|Hubei|Hunan|Jiangsu|Jiangxi|Jilin|Liaoning|Nei Mongol|Ningxia|Qinghai|Shaanxi|Shandong|Shanghai|Shanxi|Sichuan|Tianjin|Xinjiang|Xizang (Tibet)|Yunnan|Zhejiang";
+s_a[3]="Andaman and Nicobar Islands|Andhra Pradesh|Arunachal Pradesh|Assam|Bihar|Chandigarh|Chhattisgarh|Dadra and Nagar Haveli|Daman and Diu|Delhi|Goa|Gujarat|Haryana|Himachal Pradesh|Jammu and Kashmir|Jharkhand|Karnataka|Kerala|Lakshadweep|Madhya Pradesh|Maharashtra|Manipur|Meghalaya|Mizoram|Nagaland|Orissa|Pondicherry|Punjab|Rajasthan|Sikkim|Tamil Nadu|Telangana|Tripura|Uttar Pradesh|Uttaranchal|West Bengal";
+s_a[4]="Balochistan|Federally Administered Tribal Areas|Islamabad Capital Territory|North-West Frontier Province|Punjab|Sindh";
+s_a[5]="Alabama|Alaska|Arizona|Arkansas|California|Colorado|Connecticut|Delaware|District of Columbia|Florida|Georgia|Hawaii|Idaho|Illinois|Indiana|Iowa|Kansas|Kentucky|Louisiana|Maine|Maryland|Massachusetts|Michigan|Minnesota|Mississippi|Missouri|Montana|Nebraska|Nevada|New Hampshire|New Jersey|New Mexico|New York|North Carolina|North Dakota|Ohio|Oklahoma|Oregon|Pennsylvania|Rhode Island|South Carolina|South Dakota|Tennessee|Texas|Utah|Vermont|Virginia|Washington|West Virginia|Wisconsin|Wyoming";
+
+
+
+function populateStates( countryElementId, stateElementId ){
+	
+	var selectedCountryIndex = document.getElementById( countryElementId ).selectedIndex;
+
+	var stateElement = document.getElementById( stateElementId );
+	
+	stateElement.length=0;
+	stateElement.options[0] = new Option('Select State','');
+	stateElement.selectedIndex = 0;
+	
+	var state_arr = s_a[selectedCountryIndex].split("|");
+	
+	for (var i=0; i<state_arr.length; i++) {
+		stateElement.options[stateElement.length] = new Option(state_arr[i]);
+	}
+}
+
+function populateCountries(countryElementId, stateElementId){
+	// given the id of the <select> tag as function argument, it inserts <option> tags
+	var countryElement = document.getElementById(countryElementId);
+	countryElement.length=0;
+	countryElement.options[0] = new Option('Select Country','-1');
+	countryElement.selectedIndex = 0;
+	for (var i=0; i<country_arr.length; i++) {
+		countryElement.options[countryElement.length] = new Option(country_arr[i]);
+	}
+
+	// Assigned all countries. Now assign event listener for the states.
+
+	if( stateElementId ){          //to select state only after country is selected
+		countryElement.onchange = function(){
+			populateStates( countryElementId, stateElementId );
+		};
+	}
+}
---
+++ main.js
@@ -0,0 +1,116 @@
+var empName = document.querySelector("#empname");
+var empAge = document.querySelector("#empage");
+var empCountry = document.querySelector("#country");
+var empState = document.querySelector("#state");
+
+var country = [];
+var uniqueCountry = [];
+var rowno = 0;
+
+
+function success() {
+  if ((document.getElementById("empname").value == "") || (document.getElementById("empage").value == "") || (document.getElementById("country").value == "")) {
+    document.getElementById('save').disabled = true;
+  } else {
+    document.getElementById('save').disabled = false;
+  }
+}
+
+var submitBtn = document.querySelector('#save');
+
+
+function clickEventHandler() {
+  var newDate = new Date();
+  var year = newDate.getUTCFullYear();
+  var userAge = Math.abs(year - empAge.value);
+  var tableUser = document.querySelector("#table1");
+  var row = tableUser.insertRow(-1);
+  var name = row.insertCell(0);
+  var age = row.insertCell(1);
+  var country = row.insertCell(2);
+  var state = row.insertCell(3);
+  name.innerHTML = empName.value;
+  age.innerHTML = userAge;
+  country.innerHTML = empCountry.value;
+  state.innerHTML = empState.value;
+}
+
+function countryCount() {
+  var flag = 0;
+  var k = -1;
+  //create a new table with id table1 in your index.html
+  var tds = document.getElementById('table1').querySelectorAll("tr td:nth-child(3)");
+  var newcountry = tds[tds.length - 1].textContent;
+  for (var l = 0; l < country.length; l++) {
+    if (country[l] == newcountry) {
+      flag = 1;
+      break;
+    }
+  }
+  if (flag == 0) {
+    country[country.length] = tds[tds.length - 1].textContent;
+    //means you don't have the country.
+    //just add the country and count in your table2
+    var tableUser = document.querySelector("#table2");
+    var row = tableUser.insertRow(-1);
+    var countryname = row.insertCell(0);
+    var countrynum = row.insertCell(1);
+
+    countryname.innerHTML = newcountry;
+    countrynum.innerHTML = "1";
+  } else {
+    //means you have the country. 
+    //Find the country from the new table and take the count of the country and increment that.
+    //TO GET THE COUNTRIES---->
+    var tds2 = document.getElementById('table2').querySelectorAll("tr td:first-child");
+    //GET THE ROWS POSITION WHERE IT IS using 
+    var k = 0;
+    for (var i = 0; i <= tds2.length; i++) {
+      if (tds2[i].textContent == newcountry) {
+        k = i;
+        //alert("k: "+k);
+        break;
+      }
+    }
+    if (k != -1) {
+      //That means you have the data
+      //TO GET THE COUNT----->
+      var tds1 = document.getElementById('table2').querySelectorAll("tr td:last-child");
+      //alert(tds1[0]);
+      var countofcountry = parseInt(tds1[k].textContent) + 1;
+      //alert(typeof countofcountry);
+    }
+    var table = document.getElementById('table2');
+    // Delete respective row
+    //alert("k:"+ (parseInt(k)));
+    table.deleteRow(parseInt(k));
+    if (country.length == 1) {
+      rowno = rowno + 1;
+    }
+    if (rowno == 1)
+      table.deleteRow(parseInt(0));  
+    //now remove the previous count and write the new count
+    var tableUser = document.querySelector("#table2");
+    var row = table.insertRow(-1);
+    var countryname = row.insertCell(0);
+    var countrynum = row.insertCell(1);
+    countryname.innerHTML = newcountry;
+    countrynum.innerHTML = countofcountry;
+  }
+  //
+  //country.push(tds[tds.length-1].textContent);
+  //alert(country);
+  success();
+}
+
+
+function clearAll() {
+  empName.value = ""
+  empAge.value = ""
+  empCountry.value = ""
+  empState.value = ""
+}
+
+submitBtn.addEventListener("click", clickEventHandler);
+submitBtn.addEventListener("click", clearAll);
+submitBtn.addEventListener("click", countryCount);
